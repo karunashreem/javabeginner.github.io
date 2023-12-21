f0e0a1fa0fd6a413717f636e02389f7f1f2d76fe
karunashreem <shreekarunam@gmail.com>
2023-12-21 22:44:05 +0530

    Create declare_datatypes.java

---
+++ declare_datatypes.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String args[]){
+        byte roll_no= 57;
+        short reg_no= 10057;
+        int  age= 18;
+        long adm_no= 1234567;
+        float percentage= 80.5f; //should be accompanied by a "f" after the value
+        double cgpa= 9.28;
+        char gender= 'f';
+        boolean bool= true;
+    }
+}
