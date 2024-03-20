6b83e7fadb090c2ed25286c4fb7a62a0c83b166f
karunashreem <shreekarunam@gmail.com>
2024-03-21 01:10:36 +0530

    Create maximum_repeating numbers.java

---
+++ maximum_repeating numbers.java
@@ -0,0 +1,22 @@
+
+
+class Solution {
+    int maxRepeating(int[] arr, int n, int k) {
+        // code here
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:arr){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        int max=-1;
+        int res=-1;
+        for(int i=0;i<k;i++){
+            if(map.containsKey(i)){
+                if(map.get(i)>max){
+                    max=map.get(i);
+                    res=i;
+                }
+            }
+        }
+        return res;
+    }
+}
