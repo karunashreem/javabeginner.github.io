ab8e5bfc942f3a985986e610cbb0421e65d7446e
karunashreem <shreekarunam@gmail.com>
2024-05-01 00:34:03 +0530

    Create front_to_back.java

---
+++ front_to_back.java
@@ -0,0 +1,9 @@
+void iter(vector<int>::iterator it1,vector<int>::iterator it2)
+{
+    //Your code here. Use iterator to print all the elements
+     for(auto i=it1;i<it2;i++)
+    {
+        cout<<*i<<" ";
+    }
+    cout<<endl;
+}
