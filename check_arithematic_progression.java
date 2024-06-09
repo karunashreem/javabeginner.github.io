259cc3840e6f1312e6a4dbf73931db679171e3df
karunashreem <shreekarunam@gmail.com>
2024-06-09 20:39:11 +0530

    Create check_arithematic_progression.java

---
+++ check_arithematic_progression.java
@@ -0,0 +1,16 @@
+
+class Solution
+{
+    boolean checkIsAP(int arr[] ,int n)
+    {
+        // code here
+         Arrays.sort(arr);
+           int d=arr[1]-arr[0];
+            for(int i=2;i<n;i++){
+                 if(arr[i]-arr[i-1]!=d){
+                      return false ;
+                 }
+            }
+             return  true;
+    }
+}
